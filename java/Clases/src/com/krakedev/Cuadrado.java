b0f3a974bf9a3afc4e3623fe716ec371157a4801
Draemiz <draemizgx1@gmail.com>
2026-03-22 13:09:46 -0500

    Se creo un código para sacar el resultado del area y perimetro del Cuadrado

---
+++ java/Clases/src/com/krakedev/Cuadrado.java
@@ -0,0 +1,13 @@
+package com.krakedev;
+
+public class Cuadrado {
+	public int lado;
+	
+	public int calcularArea() {
+		return lado * lado;
+	}
+	
+	public int calcularPerimetro() {
+		return lado * 4;
+	}
+}
---
+++ java/Clases/src/com/krakedev/TestCuadrado.java
@@ -0,0 +1,43 @@
+package com.krakedev;
+
+public class TestCuadrado {
+
+	public static void main(String[] args) {
+		Cuadrado c1 = new Cuadrado();
+		Cuadrado c2 = new Cuadrado();
+		Cuadrado c3 = new Cuadrado();
+		
+		int area1;
+		int area2;
+		int area3;
+		int perimetro1;
+		int perimetro2;
+		int perimetro3;
+		
+		c1.lado = 4;
+		c2.lado = 5;
+		c3.lado = 6;
+		
+		area1 = c1.calcularArea();
+		area2 = c2.calcularArea();
+		area3 = c3.calcularArea();
+		
+		perimetro1 = c1.calcularPerimetro();
+		perimetro2 = c2.calcularPerimetro();
+		perimetro3 = c3.calcularPerimetro();
+		
+		System.out.println("|- CUADRADO =========|");
+
+        System.out.println("Area c1: " + area1);
+        System.out.println("Area c2: " + area2);
+        System.out.println("Area c3: " + area3);
+
+        System.out.println("|====================|");
+        System.out.println("Perimetro c1: " + perimetro1);
+        System.out.println("Perimetro c2: " + perimetro2);
+        System.out.println("Perimetro c3: " + perimetro3);
+
+		
+	}
+
+}
