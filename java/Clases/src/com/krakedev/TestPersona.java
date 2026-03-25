bd93a235ac7593c9f4a65fe0adb3062422812b13
Draemiz <draemizgx1@gmail.com>
2026-03-24 22:38:44 -0500

    aprendiendo contructores

--- java/Clases/src/com/krakedev/TestPersona.java
+++ java/Clases/src/com/krakedev/TestPersona.java
@@ -5,7 +5,14 @@ public class TestPersona {
 	public static void main(String[] args) {
 		Persona p; // 1. Declaro una variable llamada p de tipo persona
 		Persona p2 = new Persona();
+		Persona p3 = new Persona("Pablo");
+		Persona p4 = new Persona("Rocio",43,1.60);
+		
 		p = new Persona(); //Instanciar o crear un objeto persona, referenciarlo con p
+		
+		System.out.println("nombre: " + p4.getNombre());
+		System.out.println("edad: " + p4.getEdad());
+		System.out.println("estatura: " + p4.getEstatura());
 		
 		//3. Accedo a los atributos
 		System.out.println("nombre: " + p.getNombre());
